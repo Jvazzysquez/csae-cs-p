b0519674e157cec2b74cfbf8e6c6218a0cbcffd8
Jvazzysquez <jacob.g.vazquez@gmail.com>
2026-09-08 18:23:35 +0000

    My first commit

---
+++ crash-course.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+a=4
+b=5.5
+c="CSAEA"
+d=False
+
+print(a, b, c, d)
+
+e= 4*70
+print(e)
+e+=7
+print(e)
+print(f"HI how u doing e is equal to {e}")
+e+=13
+print(f"e now is {e}")
